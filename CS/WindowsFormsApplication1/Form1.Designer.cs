bfe09b0e9ce616d39763711b3bfcbffc0650af5f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Timeline;
+            this.schedulerControl1.Location = new System.Drawing.Point(12, 12);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(944, 545);
+            this.schedulerControl1.Start = new System.DateTime(2013, 3, 14, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            timeRuler1.TimeZoneId = "Russian Standard Time";
+            timeRuler1.UseClientTimeZone = false;
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
+            timeRuler2.TimeZoneId = "Russian Standard Time";
+            timeRuler2.UseClientTimeZone = false;
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2);
+            this.schedulerControl1.AppointmentDrag += new DevExpress.XtraScheduler.AppointmentDragEventHandler(this.schedulerControl1_AppointmentDrag);
+            this.schedulerControl1.AppointmentDrop += new DevExpress.XtraScheduler.AppointmentDragEventHandler(this.schedulerControl1_AppointmentDrop);
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.schedulerControl1);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(968, 569);
+            this.layoutControl1.TabIndex = 1;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.CustomizationFormText = "layoutControlGroup1";
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(968, 569);
+            this.layoutControlGroup1.Text = "layoutControlGroup1";
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.schedulerControl1;
+            this.layoutControlItem1.CustomizationFormText = "layoutControlItem1";
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(948, 549);
+            this.layoutControlItem1.Text = "layoutControlItem1";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextToControlDistance = 0;
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 569);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+    }
+}
+
---
+++ VB/WindowsFormsApplication1/Form1.vb
@@ -0,0 +1,71 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraScheduler
+
+Namespace WindowsFormsApplication1
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private minTime As New TimeSpan(8, 0, 0)
+        Private maxTime As New TimeSpan(19, 0, 0)
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            Dim scales As TimeScaleCollection = schedulerControl1.TimelineView.Scales
+            schedulerControl1.TimelineView.Scales.BeginUpdate()
+            Try
+                scales.Clear()
+                scales.Add(New MyTimeScaleFixedInterval(TimeSpan.FromDays(1), minTime, maxTime))
+                scales.Add(New MyTimeScaleFixedInterval(TimeSpan.FromHours(1), minTime, maxTime))
+                scales.Add(New MyTimeScaleFixedInterval(TimeSpan.FromMinutes(30), minTime, maxTime))
+            Finally
+                schedulerControl1.TimelineView.Scales.EndUpdate()
+            End Try
+
+            Dim apt As Appointment = schedulerStorage1.CreateAppointment(AppointmentType.Normal)
+            apt.Start = Date.Today.AddHours(10)
+            apt.End = Date.Today.AddHours(14)
+            apt.Subject = "Test appointment"
+            schedulerStorage1.Appointments.Add(apt)
+        End Sub
+
+        Private Sub CorrectAppointmentParameters(ByVal e As AppointmentDragEventArgs)
+            Dim customScaleDuration As TimeSpan = (maxTime.Subtract(minTime)).Add(TimeSpan.FromHours(2))
+            Dim initialStart As Date = e.EditedAppointment.Start
+
+            If e.EditedAppointment.Start.TimeOfDay < minTime OrElse e.EditedAppointment.Start.TimeOfDay >= maxTime Then
+                Dim durationMustBeIncreased As Boolean = e.EditedAppointment.Duration > customScaleDuration
+                e.EditedAppointment.Start = If(durationMustBeIncreased, e.EditedAppointment.Start + customScaleDuration, e.EditedAppointment.Start - customScaleDuration)
+
+                Dim appInterval As New TimeInterval(e.EditedAppointment.Start, e.EditedAppointment.Start + e.EditedAppointment.Duration + customScaleDuration)
+                If Not appInterval.Contains(e.HitInterval) Then
+                    durationMustBeIncreased = Not durationMustBeIncreased
+                    e.EditedAppointment.Start = If(durationMustBeIncreased, initialStart.Add(customScaleDuration), initialStart.Subtract(customScaleDuration))
+                End If
+                e.Handled = True
+            End If
+
+            If e.EditedAppointment.End.TimeOfDay >= maxTime OrElse e.EditedAppointment.End.TimeOfDay < minTime Then
+                e.EditedAppointment.End = If(e.EditedAppointment.Duration > customScaleDuration, e.EditedAppointment.End - customScaleDuration, e.EditedAppointment.End + customScaleDuration)
+                e.Handled = True
+            End If
+        End Sub
+
+        Private Sub schedulerControl1_AppointmentDrag(ByVal sender As Object, ByVal e As AppointmentDragEventArgs) Handles schedulerControl1.AppointmentDrag
+            CorrectAppointmentParameters(e)
+        End Sub
+
+        Private Sub schedulerControl1_AppointmentDrop(ByVal sender As Object, ByVal e As AppointmentDragEventArgs) Handles schedulerControl1.AppointmentDrop
+            CorrectAppointmentParameters(e)
+        End Sub
+    End Class
+End Namespace
