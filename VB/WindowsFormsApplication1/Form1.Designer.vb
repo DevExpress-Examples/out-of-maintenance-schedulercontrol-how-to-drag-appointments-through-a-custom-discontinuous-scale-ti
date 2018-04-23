957e00bb68cc868860f48652905d11684747e2db
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WindowsFormsApplication1/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsFormsApplication1
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
+			Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+			Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+			Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+			Me.layoutControl1 = New DevExpress.XtraLayout.LayoutControl()
+			Me.layoutControlGroup1 = New DevExpress.XtraLayout.LayoutControlGroup()
+			Me.layoutControlItem1 = New DevExpress.XtraLayout.LayoutControlItem()
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.layoutControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.layoutControl1.SuspendLayout()
+			CType(Me.layoutControlGroup1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.layoutControlItem1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' schedulerControl1
+			' 
+			Me.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Timeline
+			Me.schedulerControl1.Location = New System.Drawing.Point(12, 12)
+			Me.schedulerControl1.Name = "schedulerControl1"
+			Me.schedulerControl1.Size = New System.Drawing.Size(944, 545)
+			Me.schedulerControl1.Start = New System.DateTime(2013, 3, 14, 0, 0, 0, 0)
+			Me.schedulerControl1.Storage = Me.schedulerStorage1
+			Me.schedulerControl1.TabIndex = 0
+			Me.schedulerControl1.Text = "schedulerControl1"
+			timeRuler1.TimeZone.DaylightBias = System.TimeSpan.Parse("-01:00:00")
+			timeRuler1.TimeZone.DaylightZoneName = "Russian Daylight Time"
+			timeRuler1.TimeZone.DisplayName = "(UTC+04:00) Moscow, St. Petersburg, Volgograd"
+			timeRuler1.TimeZone.StandardZoneName = "Russian Standard Time"
+			timeRuler1.TimeZone.UtcOffset = System.TimeSpan.Parse("04:00:00")
+			timeRuler1.UseClientTimeZone = False
+			Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
+			timeRuler2.TimeZone.DaylightBias = System.TimeSpan.Parse("-01:00:00")
+			timeRuler2.TimeZone.DaylightZoneName = "Russian Daylight Time"
+			timeRuler2.TimeZone.DisplayName = "(UTC+04:00) Moscow, St. Petersburg, Volgograd"
+			timeRuler2.TimeZone.StandardZoneName = "Russian Standard Time"
+			timeRuler2.TimeZone.UtcOffset = System.TimeSpan.Parse("04:00:00")
+			timeRuler2.UseClientTimeZone = False
+			Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+'			Me.schedulerControl1.AppointmentDrag += New DevExpress.XtraScheduler.AppointmentDragEventHandler(Me.schedulerControl1_AppointmentDrag);
+'			Me.schedulerControl1.AppointmentDrop += New DevExpress.XtraScheduler.AppointmentDragEventHandler(Me.schedulerControl1_AppointmentDrop);
+			' 
+			' layoutControl1
+			' 
+			Me.layoutControl1.Controls.Add(Me.schedulerControl1)
+			Me.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.layoutControl1.Location = New System.Drawing.Point(0, 0)
+			Me.layoutControl1.Name = "layoutControl1"
+			Me.layoutControl1.Root = Me.layoutControlGroup1
+			Me.layoutControl1.Size = New System.Drawing.Size(968, 569)
+			Me.layoutControl1.TabIndex = 1
+			Me.layoutControl1.Text = "layoutControl1"
+			' 
+			' layoutControlGroup1
+			' 
+			Me.layoutControlGroup1.CustomizationFormText = "layoutControlGroup1"
+			Me.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True
+			Me.layoutControlGroup1.GroupBordersVisible = False
+			Me.layoutControlGroup1.Items.AddRange(New DevExpress.XtraLayout.BaseLayoutItem() { Me.layoutControlItem1})
+			Me.layoutControlGroup1.Location = New System.Drawing.Point(0, 0)
+			Me.layoutControlGroup1.Name = "layoutControlGroup1"
+			Me.layoutControlGroup1.Size = New System.Drawing.Size(968, 569)
+			Me.layoutControlGroup1.Text = "layoutControlGroup1"
+			Me.layoutControlGroup1.TextVisible = False
+			' 
+			' layoutControlItem1
+			' 
+			Me.layoutControlItem1.Control = Me.schedulerControl1
+			Me.layoutControlItem1.CustomizationFormText = "layoutControlItem1"
+			Me.layoutControlItem1.Location = New System.Drawing.Point(0, 0)
+			Me.layoutControlItem1.Name = "layoutControlItem1"
+			Me.layoutControlItem1.Size = New System.Drawing.Size(948, 549)
+			Me.layoutControlItem1.Text = "layoutControlItem1"
+			Me.layoutControlItem1.TextSize = New System.Drawing.Size(0, 0)
+			Me.layoutControlItem1.TextToControlDistance = 0
+			Me.layoutControlItem1.TextVisible = False
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(968, 569)
+			Me.Controls.Add(Me.layoutControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load);
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.layoutControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.layoutControl1.ResumeLayout(False)
+			CType(Me.layoutControlGroup1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.layoutControlItem1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private WithEvents schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+		Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+		Private layoutControl1 As DevExpress.XtraLayout.LayoutControl
+		Private layoutControlGroup1 As DevExpress.XtraLayout.LayoutControlGroup
+		Private layoutControlItem1 As DevExpress.XtraLayout.LayoutControlItem
+	End Class
+End Namespace
+
